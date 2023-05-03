cdaf016621e3d0860f804a77808ef6f4bb7be0c0
PrathamShah29 <119159666+PrathamShah29@users.noreply.github.com>
2023-05-03 14:11:12 +0530

    Dynamic Programming Longest Common Substring

---
+++ LongestCommonSubstring.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class LongestCommonSubstring {
+
+    public static int getLCSubstring(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                    ans = Math.max(ans, dp[i][j]);
+                }
+            }
+
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.nextLine();
+        String s2 = sc.nextLine();
+        System.out.println(getLCSubstring(s1, s2));
+
+    }
+}
